09a7d6259ad156d9c12e896c8a9d9c04bff79e7d
Ross <ross.serino@inforeliance.com>
2016-11-28 18:21:02 -0500

    added goals / objectives

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import FacebookLogin from 'react-facebook-login';
 
+function Goal(props) {
+  return <h1><b>{props.name}</b></h1>
+}
+function Objective(props) {
+  return <h2>{props.name}</h2>
+}
+
 class Login extends Component {
   responseFacebook = (response) => {
     console.log(response);
@@ -21,6 +28,11 @@ class Login extends Component {
           scope="public_profile,user_friends,user_actions.books"
           callback={this.responseFacebook}
         />
+        <Goal name="Together, we can" />
+        <Objective name="Reduce energy, water, waste consumption" />
+        <Objective name="Reduce pollution & CO2 Emissions" />
+        <Objective name="Improve Air & Water Quality" />
+        <Objective name="Improve Citizen Prosperity, equity, education, healthy, safety, and wellness" />
     </div>
   );
   }
