f185f5d07c23528ec4d1e854deb3cfd2a0a60ccc
Michael J Smith <michael.j.smith@slalom.com>
2016-11-28 15:01:03 -0800

    Added lambda deployment to CI script

--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -11,3 +11,8 @@ npm run build
 cd /home/ec2-user/usgbc/build
 
 aws s3 sync . s3://usgbc-table41
+
+cd /home/ec2-user/usgbc/api
+
+zip getSurveyById.zip getSurveyById.py
+aws lambda update-function-code --function-name getSurveyById --zip-file getSurveyById.zip
