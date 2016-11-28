7b38afc97b2492d6cdfd6f874e99ccbd83f63f26
Aydrian J. Howard <aydrian@gmail.com>
2016-11-28 11:54:29 -0800

    added survey routes

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,16 @@ import { Router, Route } from 'react-router';
 
 import App from './components/App';
 import NotFound from './components/NotFound';
+import CreateSurvey from './components/CreateSurvey';
+import TakeSurvey from './components/TakeSurvey';
+import ViewSurveyResults from './components/ViewSurveyResults';
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={App} />
+    <Route path="/survey/create" component={CreateSurvey} />
+    <Route path="/survey/:surveyId/" component={TakeSurvey} />
+    <Route path="/survey/:surveyId/results" component={ViewSurveyResults} />
     <Route path="*" component={NotFound} />
   </Router>
 );
