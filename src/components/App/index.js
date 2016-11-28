57c54343993d1e9e64af91a89a24066ccdcd0d5a
Aydrian J. Howard <aydrian@gmail.com>
2016-11-28 14:31:54 -0800

    Facebook login

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import FacebookLogin from 'react-facebook-login';
 
 import logo from './logo.svg';
 import './style.css';
 
 class App extends Component {
-  // static propTypes = {}
-  // static defaultProps = {}
-  // state = {}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  responseFacebook = (response) => {
+    console.log(response);
+    this.setState({
+      fbObject: response
+    });
+  };
 
   render() {
     const className = this.props.className;
+    console.log(this.state);
     return (
       <div className={classnames('App', className)}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>LEED City</h2>
         </div>
-        <p className="App-intro">
-          Nothing to see here, run along.<br />
-          Updated.
-        </p>
+        <img src={this.state.fbObject ? this.state.fbObject.picture.data.url : ''} alt="facebook" />
+        <FacebookLogin
+          appId="211863512589812"
+          autoLoad={true}
+          fields="name,email,picture"
+          scope="public_profile,user_friends,user_actions.books"
+          callback={this.responseFacebook}
+        />
       </div>
     );
   }
