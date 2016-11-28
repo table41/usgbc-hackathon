217302d971572b6c87cd34000bfee819a7852efe
Michael J Smith <michael.j.smith@slalom.com>
2016-11-28 15:26:28 -0800

    Fixed deployment error

--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -15,4 +15,4 @@ aws s3 sync . s3://usgbc-table41
 cd /home/ec2-user/usgbc/api
 
 zip getSurveyById.zip getSurveyById.py
-aws lambda update-function-code --function-name getSurveyById --zip-file getSurveyById.zip
+aws lambda update-function-code --function-name getSurveyById --zip-file fileb://getSurveyById.zip
