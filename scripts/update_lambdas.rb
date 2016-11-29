d3eced457f0258ca0c529cb5eb7db9f224128dcc
Dan Rosenbloom <drosenbloom@logicworks.net>
2016-11-28 18:46:02 -0800

    fix deploy

--- scripts/update_lambdas.rb
+++ scripts/update_lambdas.rb
@@ -5,13 +5,13 @@ Dir.chdir('/home/ec2-user/usgbc/api')
 
 `rm -f usgbc.zip` if File.exist?('usgbc.zip')
 `pip install geopy -t .`
-`zip usgbc.zip -r .`
+puts `zip usgbc.zip -r .`
 
 lambda = Aws::Lambda::Client.new(region: 'us-west-2')
 
 Dir.glob('*.py').each do |file|
   function_name = file.split('.')[0]
-  handler = lambda.get_function_configuration(function_name: 'getUserScore').handler
+  handler = lambda.get_function_configuration(function_name: function_name).handler
   unless handler == "#{function_name}.lambda_handler"
     lambda.update_function_configuration(function_name: function_name, handler: "#{function_name}.lambda_handler")
     puts "Updated handler on #{function_name}"
