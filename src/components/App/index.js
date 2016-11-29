c0149206c310eccd822fa15f1ab758d37321f37e
Ross <ross.serino@inforeliance.com>
2016-11-28 19:18:51 -0500

    updated packages / handled login differently on each page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-
+import cookie from 'react-cookie';
+import FacebookLogin from 'react-facebook-login';
 import logo from './logo.png';
 import './style.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    if (!this.state) {
+      this.state = {}
+      var cookieState = cookie.load('fbObject');
+    }
+
   }
   
   render() {
     const className = this.props.className;
     const {children} = this.props;
     console.log(this.state);
+    this.responseFacebook = (response) => {
+      console.log(response);
+      this.setState({
+        fbObject: response
+      });
+      
+    };
+
     return (
       <div className={classnames('App', className)}>
         <div className="App-header">
@@ -22,6 +35,14 @@ class App extends Component {
         </div>
         <img src={this.state.fbObject ? this.state.fbObject.picture.data.url : ''} alt="facebook" />
         {children}
+        <div className={this.state.fbObject ? 'hidden' : ''}>
+        <FacebookLogin
+          appId="211863512589812"
+          autoLoad={true}
+          fields="name,email,picture"
+          scope="public_profile,user_friends,user_actions.books"
+          callback={this.responseFacebook} />
+        </div>
       </div>
     );
   }
