0d9015d46d2eb9acb7435a3177071d02dd1120e7
Michael J Smith <michael.j.smith@slalom.com>
2016-11-28 16:27:28 -0800

    Updated CD script

--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -14,8 +14,13 @@ aws s3 sync . s3://usgbc-table41
 
 cd /home/ec2-user/usgbc/api
 
+pip intall geopy -t .
+
 zip getSurveyById.zip getSurveyById.py
 aws lambda update-function-code --region us-west-2 --function-name getSurveyById --zip-file fileb://getSurveyById.zip
 
-zip getSurveys.zip getSurveys.py
+zip getSurveys.zip -r geopy getSurveys.py
 aws lambda update-function-code --region us-west-2 --function-name getSurveys --zip-file fileb://getSurveys.zip
+
+zip postSurveys.zip postSurveys.py
+aws lambda update-function-code --region us-west-2 --function-name postSurveys --zip-file fileb://postSurveys.zip
