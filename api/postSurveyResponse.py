98c47ca50c7ae13ba96df7f3096ea9300a102f00
Michael Fuller <mfuller@MFuller.local>
2016-11-28 19:12:35 -0800

    moved mapping name conversion code to helper file

--- api/postSurveyResponse.py
+++ api/postSurveyResponse.py
@@ -7,15 +7,6 @@ import boto3
 import googlemaps
 import requests
 
-def get_city_name(latitude, longitude):
-    location_str = 'https://maps.googleapis.com/maps/api/geocode/json?latlng={0},{1}'.format(latitude,longitude)
-    response = requests.get(location_str)
-    resp_json_payload = response.json()
-    address = resp_json_payload['results'][0]['address_components']
-    for i in address:
-        if 'locality' in i['types']:
-            return i['short_name']
-
 def lambda_handler(event, context):
     try:
         print("Received event: " + json.dumps(event, indent=2))
