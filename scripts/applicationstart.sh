333c61f2868d9a504ecb98c56dc634c9e272e0bb
Dan Rosenbloom <drosenbloom@logicworks.net>
2016-11-28 18:20:28 -0800

    improved deployment

--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -11,19 +11,3 @@ npm run build
 cd /home/ec2-user/usgbc/build
 
 aws s3 sync . s3://usgbc-table41
-
-cd /home/ec2-user/usgbc/api
-
-pip install geopy -t .
-
-zip getSurveyById.zip getSurveyById.py
-aws lambda update-function-code --region us-west-2 --function-name getSurveyById --zip-file fileb://getSurveyById.zip
-
-zip getSurveys.zip -r geopy* getSurveys.py
-aws lambda update-function-code --region us-west-2 --function-name getSurveys --zip-file fileb://getSurveys.zip
-
-zip postSurvey.zip postSurvey.py
-aws lambda update-function-code --region us-west-2 --function-name postSurvey --zip-file fileb://postSurvey.zip
-
-zip postSurveyResponse.zip postSurveyResponse.py
-aws lambda update-function-code --region us-west-2 --function-name postSurveyResponse --zip-file fileb://postSurveyResponse.zip
